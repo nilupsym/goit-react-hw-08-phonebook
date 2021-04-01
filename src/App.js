57d5d84b3661e5dd72ed3b0c6f572a433540f7cf
Andriy Rudkovsky <a.rudkovskyy@gmail.com>
2021-04-01 04:30:44 +0300

    adds redux-persist (local storage)

--- src/App.js
+++ src/App.js
@@ -3,21 +3,6 @@ import Filter from './components/Filter';
 import ContactList from './components/ContactList';
 
 export default function App() {
-
-  // componentDidMount() {
-  //   const contacts = localStorage.getItem('contacts');
-  //   const parcedContacts = JSON.parse(contacts);
-  //   if (parcedContacts) {
-  //     this.setState({ contacts: parcedContacts });
-  //   }
-  // }
-
-  // componentDidUpdate(prevProps, prevState) {
-  //   if (this.state.contacts !== prevState.contacts) {
-  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-  //   }
-  // }
-
   return (
     <>
       <h1>Phonebook</h1>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,38 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware, } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
+import {
+    persistStore, persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import reducer from './reducer';
 
-const middleware = [...getDefaultMiddleware(), logger];
+const middleware = [...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }), logger];
+
+const contactsPersistConfig = {
+    key: 'contacts',
+    storage,
+    blacklist: ['filter'],
+}
 
 const store = configureStore({
     reducer: {
-        contacts: reducer,
-    },
+    contacts: persistReducer(contactsPersistConfig, reducer),
+},
     middleware,
     devTools: process.env.NODE_ENV === 'development',
 });
 
-export default store;
+const persistor = persistStore(store);
+
+// eslint-disable-next-line
+export default { store, persistor };
